71c71047a96de0df24665875cdced39374651f7f
Adrrolroj <adrrolroj@alum.us.es>
2021-02-10 10:29:22 +0100

    Commit for docker

--- decide/local_settings.py
+++ decide/local_settings.py
@@ -1,17 +1,4 @@
-DEBUG = True
-
-DATABASES = {
-'default': {
-'ENGINE': 'django.db.backends.postgresql',
-'NAME': 'pepe',
-'USER': 'decide',
-'PASSWORD': 'decide',
-'HOST': '127.0.0.1',
-'PORT': '5432', }}
-
-STATIC_ROOT = '/app/static/'
-MEDIA_ROOT = '/app/static/media/'
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 # Modules in use, commented modules that you won't use
 MODULES = [
@@ -39,3 +26,15 @@ APIS = {
     'visualizer': BASEURL,
     'voting': BASEURL,
 }
+
+DATABASES = {
+'default': {
+'ENGINE': 'django.db.backends.postgresql',
+'NAME': 'pepe',
+'USER': 'decide',
+'PASSWORD': 'decide',
+'HOST': '127.0.0.1',
+'PORT': '5432', }}
+
+# number of bits for the key, all auths should use the same number of bits
+KEYBITS = 256
